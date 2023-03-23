444274a33005e74fa4a2c09d693c826e151a3d8c
Steve Plowman <splowman@anexinet.com>
2023-03-23 18:20:41 -0500

    test

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.0" 
+      version = ">= 3.0" 
     }
     random = {
       source = "hashicorp/random"
@@ -22,8 +22,8 @@ terraform {
 }
 
 # Provider Block
-provider "azurerm" {
- features {}          
+provider "azurerm" { 
+ subscription_id = "00a77904-40a3-4221-bd30-14e1d2901a11"      
 }
 
 # Simple comment
